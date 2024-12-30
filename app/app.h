bc788df0bb95069acdc471963b5063e4d61af139
Asfodel <ivan.pijevic@gmail.com>
2024-12-30 17:19:33 +0100

    vj6

--- app/app.h
+++ app/app.h
@@ -1,6 +1,35 @@
 #pragma once
 
+#include <cstddef>
+
 namespace vsite::oop::v6 
 {
+	template <typename T>
+	T minimum(T a, T b)
+	{
+		return a < b ? a : b;
+	}
+
+	template <typename T>
+	struct point {};
+
+	template <typename T, unsigned long long siz>
+	class fixed_array
+	{
+	public:
+		unsigned long long size()
+		{
+			return siz;
+		}
+		T& operator[](int index)
+		{
+			return m_arr[index];
+		}
+
+	private:
+		T m_arr[siz];
+	};
+
+
 
 }
